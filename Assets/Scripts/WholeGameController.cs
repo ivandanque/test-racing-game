5fda5d25a583c3aae983e6c977e8ed8d08d9b3c7
ivandanque <ralphivan.danque.cics@ust.edu.ph>
2022-04-01 08:46:15 +0800

    Added Buttons when a level is finished

--- Assets/Scripts/WholeGameController.cs
+++ Assets/Scripts/WholeGameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WholeGameController : MonoBehaviour
 {
@@ -16,6 +17,19 @@ public class WholeGameController : MonoBehaviour
         if (playerStopwatch.RaceFinished && enemyStopwatch.RaceFinished)
         {
             RaceFinished = true;
+            GameObject endUI = GameObject.Find("UI").transform.GetChild(0).transform.GetChild(2).gameObject;
+            endUI.SetActive(true);
         }
+    }
+
+    public void NextLevel()
+    {
+        Debug.Log("Goes to Next Level");
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Goes to Main Menu");
+        SceneManager.LoadScene("MainMenu");
     }
 }
